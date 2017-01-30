e00394d26a91f90ab4fe720586411ecda9832eef
linted <linted90@hotmail.com>
2017-01-30 14:53:00 -0500

    added checks for python install (still needs work)

--- Install.sh
+++ Install.sh
@@ -4,9 +4,29 @@ echo "----------"
 echo "Skip Trace Instalation"
 echo "----------"
 
-read -p "Is this a client install? [Y/n] " -n 1 install
+echo "Checking for python3"
+python3 -V > /dev/null 2>&1
 
-if ["$install" == "n" || "$install" == "N"]; then
+if [$? != 0]; then
+    read -p "Install python3? [Y/n] " -n 1 pyInstall
+    if ["$pyInstall" == "n" || "$pyInstall" == "N"]; then
+        echo "Error: Please install python then try installing again"
+        exit -1
+
+    else
+        echo "Installing python3"
+        sudo apt-get install python3 > /dev/null 2>&1
+        echo "Installing pip3"
+        sudo apt-get install python3-pip > /dev/null 2>&1
+        echo "updating pip"
+        sudo pip3 install --upgrade pip
+    fi
+
+fi
+
+read -p "Is this a client install? [Y/n] " -n 1 clientInstall
+
+if ["$clientInstall" == "n" || "$clientInstall" == "N"]; then
     echo "Creating Server Instalation"
     echo "Moving server script to /root"
     sudo cp server/server.py /root/.
@@ -25,6 +45,10 @@ else
     sudo systemctl daemon-reload
     sudo systemctl enable locationLog.service
 fi
+
+echo "Installing python libraries"
+
+
 
 echo "exiting"
 exit 0
