088b8a27037b78942e94035d6c1ca0090769fcd5
linted <linted90@hotmail.com>
2017-01-29 21:35:38 -0500

    added preliminary server install and moved py scripts to right local

--- Install.sh
+++ Install.sh
@@ -4,12 +4,27 @@ echo "----------"
 echo "Skip Trace Instalation"
 echo "----------"
 
-echo "Is this a client install? [Y/n] "
+read -p "Is this a client install? [Y/n] " -n 1 install
 
-echo "Creating locationLog service."
-sudo cp client/locationLog.service /lib/systemd/system/.
-sudo systemctl daemon-reload
-sudo systemctl enable locationLog.service
+if ["$install" == "n" || "$install" == "N"]; then
+    echo "Creating Server Instalation"
+    echo "Moving server script to /root"
+    sudo cp server/server.py /root/.
+
+    echo "Setting up server locationLog service."
+    sudo cp server/locationLog.service /lib/systemd/system/.
+    sudo systemctl daemon-reload
+    sudo systemctl enable locationLog.service
+else
+    echo "Creating Server Instalation"
+    echo "Moving server script to /root"
+    sudo cp server/server.py /root/.
+    
+    echo "Creating client locationLog service."
+    sudo cp client/locationLog.service /lib/systemd/system/.
+    sudo systemctl daemon-reload
+    sudo systemctl enable locationLog.service
+fi
 
 echo "exiting"
 exit 0
